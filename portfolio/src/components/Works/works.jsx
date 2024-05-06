b84261bc606cd44c82eb39bb99b1fb56ca0f52d0
Sailesh1993 <karki.saileshnep@gmail.com>
2024-05-06 22:14:44 +0300

    create object project in the work.jsx and display i the card, modify card styles in work.css.

--- portfolio/src/components/Works/works.jsx
+++ portfolio/src/components/Works/works.jsx
@@ -8,21 +8,63 @@ import Portfolio5 from '../../assets/portfolio5.png'
 import Portfolio6 from '../../assets/portfolio6.png'
 
 const Works = () => {
+  const projects = [
+    {
+      title: "Countries App",
+      image: Portfolio1,
+      description: "Countries App fetches countries from a REST API and displays them in a table.",
+      link: "https://saileshcountryapp.netlify.app/"
+    },
+    {
+      title: "Breweries App",
+      image: Portfolio2,
+      description: "The breweries app fetches a list of products from a REST API and displays them in a table.",
+      link: "https://sailesh-product-list-app.netlify.app"
+    },
+    {
+      title: "Ecommerce App",
+      image: Portfolio3,
+      description: "An E-commerce app that fetches the list of products from a REST API and displays them in a card.",
+      link: "https://sailesh-ecommerce-project-integrify.netlify.app/"
+    },
+    {
+      title: "Ecommerce-Web App Backend",
+      image: Portfolio4,
+      description: "An E-commerce management app is a comprehensive Fullstack project involving TypeScript,Asp.NetCore, Entity Framework Core and PostgreSQL on the backend.",
+      link: "https://saileshecom-app.azurewebsites.net/swagger/index.html"
+    },
+    {
+      title: "Ecommerce-Web App Frontend",
+      image: Portfolio5,
+      description: "This project features CRUD operation for User functionalities, Admin functionalities, Clean architecture, Rest API, conventions and testing.",
+      link: "https://saileshe-ecom-webshop.netlify.app"
+    },
+    {
+      title: "Todo-List App",
+      image: Portfolio6,
+      description: "create to-do list, save for reference, edit and delete feature.",
+      link: "https://sailesh1993.github.io/fs15_3-todo-app/"
+    },
+
+  ]
   return (
     <section id='works'>
-        <h2 className='worksTitle'>My Portfolio</h2>
-        <span className='worksDesc'>I take pride in paying attention to the smallest 
-        details and making sure that my work is pixel perfect. I am excited to bring my skills 
-            and experience to help businesses achieve their goals and create a strong 
-            online presence.</span>
-        <div className='worksImgs'>
-            <a href='https://saileshcountryapp.netlify.app/' target="_blank" rel="noopener noreferrer"><img src={Portfolio1} alt="" className='worksImg' /></a>
-            <a href='https://sailesh-product-list-app.netlify.app/' target="_blank" rel="noopener noreferrer"><img src={Portfolio2} alt="" className='worksImg' /></a>
-            <a href='https://sailesh-ecommerce-project-integrify.netlify.app/' target="_blank" rel="noopener noreferrer"><img src={Portfolio3} alt="" className='worksImg' /></a>
-            <a href='https://saileshecom-app.azurewebsites.net/swagger/index.html' target="_blank" rel="noopener noreferrer"><img src={Portfolio4} alt="" className='worksImg' /></a>
-            <a href='https://saileshe-ecom-webshop.netlify.app' target="_blank" rel="noopener noreferrer"><img src={Portfolio5} alt="" className='worksImg' /></a>
-            <a href='https://sailesh1993.github.io/fs15_3-todo-app/' target="_blank" rel="noopener noreferrer"><img src={Portfolio6} alt="" className='worksImg' /></a>
-        </div>
+      <h2 className='worksTitle'>My Portfolio</h2>
+      <span className='worksDesc'>I take pride in paying attention to the smallest
+        details and making sure that my work is pixel perfect. I am excited to bring my skills
+        and experience to help businesses achieve their goals and create a strong
+        online presence.</span>
+      <div className='worksImgs'>
+        {projects.map((project, index) => (
+          <a key={index} href={project.link} target="_blank" rel="noopener noreferrer" className='project'>
+            <div>
+              <img src={project.image} alt={project.title} className='worksImg' />
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </div>
+          </a>
+        ))}
+      </div>
     </section>
   )
 }
